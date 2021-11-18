7678777251232db48056e39290a6509d4bfd3a0e
Javiera Herrera <javierab.herrera@gmail.com>
2021-11-18 12:44:21 -0300

    conectando a github

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>simulador</h1>
+<button type="button" class="btn btn-primary">Primary</button>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
